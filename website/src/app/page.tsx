2c3b1e305d938012b9d3bd6fa7d1fb06ad53b365
Maston <22679886+mastondzn@users.noreply.github.com>
2023-05-02 02:09:23 +0300

    feat: update landing page

--- website/src/app/page.tsx
+++ website/src/app/page.tsx
@@ -5,29 +5,25 @@ import Link from 'next/link';
 import synopsisWhite from '../../public/synopsiswhite.png';
 import { Button } from '~/components/button';
 
-export default function IndexPage() {
+export default function Page() {
     return (
         <div className="container flex min-h-screen flex-col items-center justify-center gap-3">
-            <Image src={synopsisWhite} alt="synopsisbot logo" className="mb-2 w-[500px]" />
+            <Image src={synopsisWhite} className="mb-2 w-[500px]" alt="" />
             <h2 className="scroll-m-20 pb-2 text-xl font-semibold tracking-tight transition-colors first:mt-0">
                 {'Website is currently under construction ⛏️'}
             </h2>
             <div className="flex flex-row gap-10">
-                <Link href="https://twitch.tv/synopsisbot" target="_blank" rel="noreferrer">
-                    <Button variant="outline" size="lg" className="h-fit w-fit px-3 py-3">
-                        <IconBrandTwitch className="h-8 w-8" />{' '}
-                    </Button>
-                </Link>
-                <Link href="https://github.com/synopsisgg/bot" target="_blank" rel="noreferrer">
-                    <Button variant="outline" size="lg" className="h-fit w-fit px-3 py-3">
-                        <IconBrandGithub className="h-8 w-8" />{' '}
-                    </Button>
-                </Link>
-                <Link href="mailto:contact@synopsis.gg" target="_blank" rel="noreferrer">
-                    <Button variant="outline" size="lg" className="h-fit w-fit px-3 py-3">
-                        <IconMail className="h-8 w-8" />
-                    </Button>
-                </Link>
+                {[
+                    { link: 'https://twitch.tv/synopsisbot', Icon: IconBrandTwitch },
+                    { link: 'https://github.com/synopsisgg/bot', Icon: IconBrandGithub },
+                    { link: 'mailto:contact@synopsis.gg', Icon: IconMail },
+                ].map(({ link, Icon }, i) => (
+                    <Link href={link} target="_blank" rel="noreferrer" key={i}>
+                        <Button variant="outline" size="lg" className="h-fit w-fit px-5 py-3">
+                            <Icon className="h-6 w-6" />
+                        </Button>
+                    </Link>
+                ))}
             </div>
         </div>
     );
