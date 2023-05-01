d19af6afe620d8dd5c508a8ac0ab0f62ddacfd81
Maston <22679886+mastondzn@users.noreply.github.com>
2023-05-02 00:02:01 +0300

    fix: typecheck again

--- packages/db/scripts/migrate.ts
+++ packages/db/scripts/migrate.ts
@@ -3,14 +3,15 @@ import { resolve } from 'node:path';
 import { config } from 'dotenv';
 import { migrate } from 'drizzle-orm/node-postgres/migrator';
 
-import { parseEnv } from '@synopsis/env';
+import { envSchema } from '@synopsis/env';
 
 import { makeDatabase } from '~/index';
 
 config({ path: resolve(process.cwd(), '../../.env') });
 
 void (async () => {
-    const env = parseEnv(process.env);
+    const env = envSchema.parse(process.env);
+
     const { db, pool } = makeDatabase({
         host: env.DB_HOST,
         user: env.DB_USERNAME,
