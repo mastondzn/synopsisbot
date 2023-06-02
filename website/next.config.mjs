725d00886474ddeb4e67c7335467f2d0e9dee495
Maston <22679886+mastondzn@users.noreply.github.com>
2023-06-02 14:27:07 +0300

    refactor: do not output in standalone mode

--- website/next.config.mjs
+++ website/next.config.mjs
@@ -1,21 +1,12 @@
 //@ts-check
 
-import { dirname, join } from 'node:path';
-import { fileURLToPath } from 'node:url';
-
 // set process.env.SKIP_ENV_VALIDATION to something to skip validation
 import '@synopsis/env/next';
-
-const __dirname = dirname(fileURLToPath(import.meta.url));
 
 /** @type {import("next").NextConfig} */
 const config = {
     reactStrictMode: true,
-    output: 'standalone',
     transpilePackages: ['@synopsis/db', '@synopsis/scopes'],
-    experimental: {
-        outputFileTracingRoot: join(__dirname, '../'),
-    },
 };
 
 export default config;
