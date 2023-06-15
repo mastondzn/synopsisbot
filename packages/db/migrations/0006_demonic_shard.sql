a743dcf2e53dcda7861d191e178a619e3b55e631
Maston <22679886+mastondzn@users.noreply.github.com>
2023-06-15 21:46:01 +0300

    delete migrations

--- packages/db/migrations/0006_demonic_shard.sql
+++
@@ -1,1 +0,0 @@
-ALTER TABLE "local_permissions" DROP CONSTRAINT "local_permissions_channel_id_channels_twitch_id_fk";
--- packages/db/migrations/0010_careful_tag.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE "commands" ALTER COLUMN "cooldown" SET NOT NULL;
-ALTER TABLE "commands" ALTER COLUMN "global_cooldown" SET NOT NULL;
-ALTER TABLE "commands" ALTER COLUMN "permission" SET NOT NULL;
-ALTER TABLE "commands" ALTER COLUMN "global_permission" SET NOT NULL;
-ALTER TABLE "commands" ALTER COLUMN "permission_mode" SET NOT NULL;
