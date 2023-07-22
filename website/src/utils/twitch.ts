809dfefe6b4eabdd39f0a80a01ecb603852e7769
Maston <22679886+mastondzn@users.noreply.github.com>
2023-07-22 19:38:49 +0300

    revert bumping twurple

--- website/src/utils/twitch.ts
+++ website/src/utils/twitch.ts
@@ -7,20 +7,19 @@ import { env } from '@synopsis/env';
 export const authProvider = new RefreshingAuthProvider({
     clientId: env.TWITCH_CLIENT_ID,
     clientSecret: env.TWITCH_CLIENT_SECRET,
-});
-
-authProvider.onRefresh(async (userId: string, token: AccessToken) => {
-    await db.authedUser.update({
-        where: { twitchId: userId },
-        data: {
-            accessToken: token.accessToken,
-            scopes: token.scope,
-            ...(token.refreshToken ? { refreshToken: token.refreshToken } : {}),
-            ...(token.expiresIn
-                ? { expiresAt: new Date(Date.now() + token.expiresIn * 1000) }
-                : {}),
-        },
-    });
+    onRefresh: async (userId: string, token: AccessToken) => {
+        await db.authedUser.update({
+            where: { twitchId: userId },
+            data: {
+                accessToken: token.accessToken,
+                scopes: token.scope,
+                ...(token.refreshToken ? { refreshToken: token.refreshToken } : {}),
+                ...(token.expiresIn
+                    ? { expiresAt: new Date(Date.now() + token.expiresIn * 1000) }
+                    : {}),
+            },
+        });
+    },
 });
 
 export const api = new ApiClient({ authProvider });
