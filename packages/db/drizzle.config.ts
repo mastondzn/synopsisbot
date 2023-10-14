559bd476d6e901486461f5eba7838dc0d1c583b4
Maston <22679886+mastondzn@users.noreply.github.com>
2023-10-14 04:56:03 +0300

    update drizzle config

--- packages/db/drizzle.config.ts
+++ packages/db/drizzle.config.ts
@@ -5,8 +5,11 @@ import { env } from '@synopsis/env/node';
 export default {
     schema: './src/schema/index.ts',
     out: './migrations',
-    host: env.DB_HOST,
-    user: env.DB_USERNAME,
-    password: env.DB_PASSWORD,
-    database: env.DB_NAME,
+    driver: 'pg',
+    dbCredentials: {
+        host: env.DB_HOST,
+        user: env.DB_USERNAME,
+        password: env.DB_PASSWORD,
+        database: env.DB_NAME,
+    },
 } satisfies Config;
