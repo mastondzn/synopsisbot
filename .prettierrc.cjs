59c6cccac0f2fb01878d57d0e752bbb5c8bc8ec4
Maston <22679886+mastondzn@users.noreply.github.com>
2024-01-12 20:24:47 +0200

    use eslint config

--- .prettierrc.cjs
+++
@@ -1,8 +0,0 @@
-/** @satisfies {import('prettier').Options} */
-const config = {
-    printWidth: 100,
-    tabWidth: 4,
-    singleQuote: true,
-};
-
-module.exports = config;
---
+++ eslint.config.mjs
@@ -0,0 +1,7 @@
+import { defineConfig } from '@mastondzn/eslint';
+
+export default defineConfig({
+    yaml: false,
+
+    ignores: ['packages', 'bot', 'website'],
+});
