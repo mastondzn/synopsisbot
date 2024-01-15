afdc6fff77176639a73391179243f1daabcb7219
Maston <22679886+mastondzn@users.noreply.github.com>
2024-01-15 19:38:29 +0200

    add consistency check

---
+++ bot/src/modules/consistency.ts
@@ -0,0 +1,20 @@
+import { env } from '@synopsis/env/node';
+import { CronJob } from 'cron';
+
+import { defineModule } from '~/helpers/module';
+import { chat } from '~/services/chat';
+
+export default defineModule({
+    name: 'consistency',
+    register: () => {
+        if (env.NODE_ENV !== 'production') return;
+
+        CronJob.from({
+            // every 5 minutes
+            cronTime: '*/5 * * * *',
+            onTick: () => {
+                void chat.say(env.TWITCH_BOT_OWNER_USERNAME, 'consistency check');
+            },
+        });
+    },
+});
