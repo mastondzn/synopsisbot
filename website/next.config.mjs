0eda9ba7cb7c5b625804b27dd94a428cf3841f31
Maston <22679886+mastondzn@users.noreply.github.com>
2024-01-16 20:40:41 +0200

    skip env check in next, create workflow for docker images

--- website/next.config.mjs
+++ website/next.config.mjs
@@ -1,15 +1,7 @@
-// set process.env.SKIP_ENV_VALIDATION to something to skip validation
-
 import { join } from 'node:path';
 import { fileURLToPath } from 'node:url';
 
-import jiti from 'jiti';
-
-const filename = fileURLToPath(import.meta.url);
 const dirname = fileURLToPath(new URL('.', import.meta.url));
-
-const environmentPath = fileURLToPath(import.meta.resolve('@synopsis/env/next'));
-jiti(filename)(environmentPath);
 
 /** @satisfies {import("next").NextConfig} */
 const config = {
