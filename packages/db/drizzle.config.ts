0e0e0eb158eb5de1515cb4c08774eac948052b55
Maston <22679886+mastondzn@users.noreply.github.com>
2024-01-16 20:44:35 +0200

    fix ci

--- packages/db/drizzle.config.ts
+++ packages/db/drizzle.config.ts
@@ -1,12 +1,13 @@
 import { env } from '@synopsis/env/node';
 import type { Config } from 'drizzle-kit';
+import isDocker from 'is-docker';
 
 export default {
     schema: './src/schema/index.ts',
     out: './migrations',
     driver: 'pg',
     dbCredentials: {
-        host: env.DB_HOST,
+        host: isDocker() ? 'db' : 'localhost',
         user: env.DB_USERNAME,
         password: env.DB_PASSWORD,
         database: env.DB_NAME,
