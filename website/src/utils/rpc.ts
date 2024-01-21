359161598791e196d3aeaaa67b0ef01409261d6d
Maston <22679886+mastondzn@users.noreply.github.com>
2024-01-22 01:28:31 +0200

    fix: bad rpc hostname

--- website/src/utils/rpc.ts
+++ website/src/utils/rpc.ts
@@ -2,6 +2,6 @@ import type { RPC } from '@synopsis/bot/rpc';
 import { hc } from 'hono/client';
 import isDocker from 'is-docker';
 
-const host = isDocker() ? 'http://bot' : 'http://localhost:3002';
+const host = isDocker() ? 'http://bot:3002' : 'http://localhost:3002';
 
 export const rpcClient = hc<RPC>(host);
