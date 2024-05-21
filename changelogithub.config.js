dc469d4b856794976dd82dd28f1271c8999e93f0
Maston <22679886+mastondzn@users.noreply.github.com>
2024-05-21 04:58:57 +0300

    chore: add changelogithub config

---
+++ changelogithub.config.js
@@ -0,0 +1,16 @@
+export default {
+    types: {
+        feat: { title: 'Enhancements' },
+        perf: { title: 'Performance' },
+        fix: { title: 'Fixes' },
+        refactor: { title: 'Refactors' },
+        docs: { title: 'Documentation' },
+        build: { title: 'Build' },
+        types: { title: 'Types' },
+        chore: { title: 'Chore' },
+        examples: { title: 'Examples' },
+        test: { title: 'Tests' },
+        style: { title: 'Styles' },
+        ci: { title: 'CI' },
+    },
+};
