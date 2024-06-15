4cb295a054dd3dfd37e8544d3ab81f1c0bfc55be
Maston <22679886+mastondzn@users.noreply.github.com>
2024-06-15 11:36:27 +0300

    feat: add pajbot alert

---
+++ bot/src/events/pajlada.ts
@@ -0,0 +1,15 @@
+import { createEventHandler } from '~/helpers/event';
+import { chat } from '~/services/chat';
+
+export default createEventHandler({
+    event: 'PRIVMSG',
+    handler: async (message) => {
+        if (
+            message.messageText === 'pajaS 🚨 ALERT' &&
+            message.channelName === 'pajlada' &&
+            message.senderUsername === 'pajbot'
+        ) {
+            await chat.say(message.channelName, '🚨 ALERTĂ pajaConcern');
+        }
+    },
+});
