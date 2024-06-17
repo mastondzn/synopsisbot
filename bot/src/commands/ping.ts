3839eff8b0826aaaaf6c32170d6234bcda8d9d3b
Maston <22679886+mastondzn@users.noreply.github.com>
2024-06-17 12:28:33 +0300

    refactor(commands/ping): use trim template

--- bot/src/commands/ping.ts
+++ bot/src/commands/ping.ts
@@ -1,29 +1,20 @@
 import ms from 'pretty-ms';
 
 import { createCommand } from '~/helpers/command/define';
+import { trim } from '~/helpers/tags';
 import { chat } from '~/services/chat';
 
 export default createCommand({
     name: 'ping',
     description: 'Replies with pong! To ensure the bot is alive.',
     aliases: ['pong'],
-    run: async ({ parameters }) => {
-        const lines = [];
-
-        if (parameters.command === 'pong') {
-            lines.push('MrDestructoid Ping!');
-        } else {
-            lines.push('MrDestructoid Pong!');
-        }
-
-        lines.push(
-            `Uptime is ${ms(process.uptime() * 1000, {
-                unitCount: 2,
-                secondsDecimalDigits: 0,
-            })}.`,
-            `Latency is ${await chat.getLatency()}ms.`,
-        );
-
-        return { reply: lines.join(' ') };
+    run: async ({ parameters: { command } }) => {
+        return {
+            reply: trim`
+                MrDestructoid ${command === 'pong' ? 'Ping' : 'Pong'}!
+                Uptime is ${ms(process.uptime() * 1000, { unitCount: 2, secondsDecimalDigits: 0 })}.
+                Latency is ${await chat.getLatency()}ms.
+            `,
+        };
     },
 });
