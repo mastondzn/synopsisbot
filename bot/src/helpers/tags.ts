7405af155b4e0dcd8933d0f2576e54134b0e2d83
Maston <22679886+mastondzn@users.noreply.github.com>
2024-06-17 12:29:31 +0300

    feat: accept numbers in trim template tag

--- bot/src/helpers/tags.ts
+++ bot/src/helpers/tags.ts
@@ -1,4 +1,5 @@
-export function trim(strings: TemplateStringsArray, ...values: string[]): string {
+/** Trims some lines for usage in twitch chat, strips newlines and extra spaces */
+export function trim(strings: TemplateStringsArray, ...values: (string | number)[]): string {
     return strings
         .reduce((acc, str, index) => acc + str + (values[index] ?? ''), '')
         .replaceAll(/\s+/g, ' ')
