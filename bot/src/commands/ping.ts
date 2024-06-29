0032469620d28eed2dd6c9ae4d6266b234e41e1c
Maston <22679886+mastondzn@users.noreply.github.com>
2024-06-29 08:26:41 +0300

    feat(commands/ping): add memory usage

--- bot/src/commands/ping.ts
+++ bot/src/commands/ping.ts
@@ -11,9 +11,10 @@ export default createCommand({
     run: async ({ parameters: { command } }) => {
         return {
             reply: trim`
-                MrDestructoid ${command === 'pong' ? 'Ping' : 'Pong'}!
+                ${command === 'pong' ? 'Ping' : 'Pong'}!
                 Uptime is ${ms(process.uptime() * 1000, { unitCount: 2, secondsDecimalDigits: 0 })}.
                 Latency is ${await chat.getLatency()}ms.
+                Memory usage is ${Math.round(process.memoryUsage().heapUsed / 1024 / 1024)}MB.
             `,
         };
     },
