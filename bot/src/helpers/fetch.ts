d425090209940c6fb41f852b496a9391f307997b
Maston <22679886+mastondzn@users.noreply.github.com>
2024-07-03 18:18:32 +0300

    feat: make fetch wrapper support `.catch()` calls in more cases

--- bot/src/helpers/fetch.ts
+++ bot/src/helpers/fetch.ts
@@ -1,6 +1,6 @@
 import type { z } from 'zod';
 
-export type ZFetchOptions<TSchema extends z.ZodTypeAny> = RequestInit & {
+export type ZFetchOptions<TSchema extends z.ZodType<unknown> = z.ZodType<unknown>> = RequestInit & {
     url: string | URL;
     throwHttpErrors?: boolean;
     schema?: TSchema;
@@ -19,36 +19,36 @@ export class HTTPError extends Error {
     }
 }
 
-export function zfetch<TSchema extends z.ZodType<unknown> = z.ZodType<unknown>>({
-    schema,
-    url,
-    throwHttpErrors,
-    json,
-    ...rest
-}: ZFetchOptions<TSchema>): Promise<TypedResponse<z.infer<TSchema>>> & {
+export function zfetch<TSchema extends z.ZodType<unknown> = z.ZodType<unknown>>(
+    options: ZFetchOptions<TSchema>,
+): Promise<TypedResponse<z.infer<TSchema>>> & {
     json: () => Promise<z.infer<TSchema>>;
     text: () => Promise<string>;
     arrayBuffer: () => Promise<ArrayBuffer>;
     blob: () => Promise<Blob>;
 } {
-    if (json) {
-        if (rest.body) throw new TypeError('Cannot specify both json and body options');
-        if (typeof json !== 'object') throw new TypeError('"json" option must be an object');
-        rest.body = JSON.stringify(json);
-    }
-
-    const promise = fetch(url, rest).then((response) => {
-        if (throwHttpErrors && !response.ok) throw new HTTPError(response);
-        const parse = response.json.bind(response);
-        return Object.assign(response, {
-            json: async () => (schema ? await schema.parseAsync(await parse()) : await parse()),
-        });
-    });
+    const promise = performFetch(options);
 
     return Object.assign(promise, {
         json: async () => await (await promise).json(),
         text: async () => await (await promise).text(),
         blob: async () => await (await promise).blob(),
         arrayBuffer: async () => await (await promise).arrayBuffer(),
+    });
+}
+
+async function performFetch({ schema, url, throwHttpErrors, json, ...rest }: ZFetchOptions) {
+    if (json) {
+        if (rest.body) throw new TypeError('Cannot specify both json and body options');
+        if (typeof json !== 'object') throw new TypeError('"json" option must be an object');
+        rest.body = JSON.stringify(json);
+    }
+
+    return await fetch(url, rest).then((response) => {
+        if (throwHttpErrors && !response.ok) throw new HTTPError(response);
+        const parse = response.json.bind(response);
+        return Object.assign(response, {
+            json: async () => (schema ? await schema.parseAsync(await parse()) : await parse()),
+        });
     });
 }
