90c896fb5d8f1eddd0060962c0e999aaeae027a8
Maston <22679886+mastondzn@users.noreply.github.com>
2024-07-14 03:47:45 +0300

    refactor(commands/reload): remove unused command

--- bot/src/commands/index.ts
+++ bot/src/commands/index.ts
@@ -1,41 +1,27 @@
-import type { Buffer } from 'node:buffer';
 import { readdir } from 'node:fs/promises';
 
 import { Collection } from '@discordjs/collection';
 
 import type { Command } from '~/helpers/command/types';
-import { computeMetaHash } from '~/helpers/hash';
 
-export interface CommandMetadata {
-    version: Date;
-    hash: Buffer;
-}
-
-class Commands extends Collection<string, Command & { meta: CommandMetadata }> {
-    public async load({ force = false }: { force?: boolean } = {}): Promise<this> {
+class Commands extends Collection<string, Command> {
+    public async load(): Promise<void> {
         const directory = (await readdir('./src/commands')).filter((path) => path !== 'index.ts');
 
-        const version = new Date();
+        await Promise.all(
+            directory.map(async (file) => {
+                const importable = file.replace('.ts', '');
+                const existing = this.get(importable);
+                if (existing) return;
 
-        for (const file of directory) {
-            const importable = file.replace('.ts', '');
-            const existing = this.get(importable);
-            if (existing && !force) continue;
+                const imported = (await import(`./${file}`)) as { default: Command };
+                if (!imported.default.name) {
+                    throw new TypeError(`Invalid command ${file}`);
+                }
 
-            const hash = await computeMetaHash(`src/commands/${file}`);
-            if (existing && force && existing.meta.hash.equals(hash)) continue;
-
-            let path = `./${importable}`;
-            // bypasses the esm cache when reloading
-            if (force) path += `?version=${version.getTime()}`;
-
-            const imported = (await import(path)) as { default: Command };
-            if (!imported.default.name) throw new TypeError(`Invalid command ${file}`);
-
-            this.set(file, Object.assign(imported.default, { meta: { version, hash } }));
-        }
-
-        return this;
+                this.set(file, imported.default);
+            }),
+        );
     }
 
     public findByName(name: string): Command | undefined {
